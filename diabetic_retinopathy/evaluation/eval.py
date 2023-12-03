54b43f4912e856a0ef8b6e2870112549d70965af
yang cao <st186635@stud.uni-stuttgart.de>
2023-12-03 04:14:03 +0100

    新增evaluate部分
    but i dont know whether it is richtig

--- diabetic_retinopathy/evaluation/eval.py
+++ diabetic_retinopathy/evaluation/eval.py
@@ -1,5 +1,26 @@
-import tensorflow as tf
+from evaluation.metrics import ConfusionMatrix
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 def evaluate(model: object, checkpoint: object, ds_test: object, ds_info: object, run_paths: object) -> object:
+    model.load_weights(checkpoint)
 
+    # 初始化混淆矩阵度量
+    confusion_matrix_metric = ConfusionMatrix(num_classes=ds_info.features['label'].num_classes)
+
+    # 在测试数据集上评估模型
+    for x, y in ds_test:
+        y_pred = model.predict(x)
+        confusion_matrix_metric.update_state(y, y_pred)
+
+    # 获取混淆矩阵
+    conf_matrix = confusion_matrix_metric.result().numpy()
+
+    # 绘制混淆矩阵
+    plt.figure(figsize=(10, 8))
+    sns.heatmap(conf_matrix, annot=True, fmt='g', cmap='Blues')
+    plt.xlabel('Predicted labels')
+    plt.ylabel('True labels')
+    plt.title('Confusion Matrix')
+    plt.show()
     return
--- diabetic_retinopathy/evaluation/metrics.py
+++ diabetic_retinopathy/evaluation/metrics.py
@@ -1,13 +1,30 @@
 import tensorflow as tf
 
+import tensorflow as tf
+import numpy as np
+
 class ConfusionMatrix(tf.keras.metrics.Metric):
-
-    def __init(self, name="confusion_matrix", **kwargs):
+    def __init__(self, num_classes, name='confusion_matrix', **kwargs):
         super(ConfusionMatrix, self).__init__(name=name, **kwargs)
-        # ...
+        self.num_classes = num_classes
+        self.confusion_matrix = self.add_weight(name='conf_matrix', shape=(num_classes, num_classes),
+                                                initializer='zeros', dtype=tf.int32)
 
-    def update_state(self, *args, **kwargs):
-        # ...
+    def update_state(self, y_true, y_pred, sample_weight=None):
+        y_true = tf.cast(y_true, self.dtype)
+        y_pred = tf.cast(tf.argmax(y_pred, axis=1), self.dtype)
+        if sample_weight is not None:
+            sample_weight = tf.cast(sample_weight, self.dtype)
+        else:
+            sample_weight = tf.ones_like(y_true)
+
+        current_cm = tf.math.confusion_matrix(y_true, y_pred, self.num_classes, weights=sample_weight, dtype=tf.int32)
+        return self.confusion_matrix.assign_add(current_cm)
 
     def result(self):
-        # ...
+        return tf.identity(self.confusion_matrix)
+
+    def reset_states(self):
+        tf.keras.backend.set_value(self.confusion_matrix, np.zeros((self.num_classes, self.num_classes), dtype=np.int32))
+
+
