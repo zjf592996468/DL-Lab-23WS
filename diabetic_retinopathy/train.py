1472b6aff604f518ab33aa5ecd5f9640d18ee7f7
yang cao <st186635@stud.uni-stuttgart.de>
2023-12-09 00:36:54 +0100

    再次调整ckpts

--- diabetic_retinopathy/train.py
+++ diabetic_retinopathy/train.py
@@ -6,14 +6,12 @@ import logging
 class Trainer(object):
     def __init__(self, model, ds_train, ds_val, ds_info, run_paths, total_steps, log_interval, ckpt_interval):
         # Summary Writer
-        # Checkpoint Manager
-        self.ckpt = tf.train.Checkpoint(model=model, optimizer=tf.keras.optimizers.Adam())
-        self.manager = tf.train.CheckpointManager(self.ckpt, run_paths['path_ckpts_train'], max_to_keep=3)
-
         # Loss objective
         self.loss_object = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
         self.optimizer = tf.keras.optimizers.Adam()
-
+        # Checkpoint Manager
+        self.ckpt = tf.train.Checkpoint(model=model, optimizer=self.optimizer)
+        self.manager = tf.train.CheckpointManager(self.ckpt, run_paths['path_ckpts_train'], max_to_keep=3)
         # Metrics
         self.train_loss = tf.keras.metrics.Mean(name='train_loss')
         self.train_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='train_accuracy')
@@ -47,18 +45,8 @@ class Trainer(object):
         self.train_accuracy(labels, predictions)
 
     @tf.function
-    def val_step(self, images, labels):
-        # training=False is only needed if there are layers with different
-        # behavior during training versus inference (e.g. Dropout).
-        predictions = self.model(images, training=False)
-        t_loss = self.loss_object(labels, predictions)
-
-        self.val_loss(t_loss)
-        self.val_accuracy(labels, predictions)
-
-
-
     def train(self):
+        start_step = self.ckpt.step.numpy()
         for idx, (images, labels) in enumerate(self.ds_train):
 
             step = idx + 1
@@ -79,28 +67,26 @@ class Trainer(object):
                                              self.train_accuracy.result() * 100,
                                              self.val_loss.result(),
                                              self.val_accuracy.result() * 100))
-                
+
                 # Write summary to tensorboard
+                # ...
+
+                # Reset train metrics
                 self.train_loss.reset_states()
                 self.train_accuracy.reset_states()
 
                 yield self.val_accuracy.result().numpy()
+
             if step % self.ckpt_interval == 0:
-                self.manager.save()
-                logging.info(f'Saving checkpoint to {self.run_paths["path_ckpts_train"]}.')
+                save_path = self.manager.save()
+                logging.info(f'Saving checkpoint to {save_path} at step {step}.')
                 # Save checkpoint
-
-
+                # ...
 
             if step % self.total_steps == 0:
                 logging.info(f'Finished training after {step} steps.')
-                # Save final checkpoint
-                # ...
-                self.manager.save()
-                logging.info(f'Saving checkpoint to {self.run_paths["path_ckpts_train"]}.')
-                # 计算并返回验证集上的准确率
+                save_path = self.manager.save()  # 保存最终的检查点
+                logging.info(f'Saving final checkpoint to {save_path}.')
                 val_accuracy = self.val_accuracy.result().numpy()
                 logging.info(f'Validation accuracy: {val_accuracy * 100:.2f}%')
-
                 return self.val_accuracy.result().numpy()
-
