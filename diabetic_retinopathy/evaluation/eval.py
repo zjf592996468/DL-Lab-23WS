d66a6473f40f387dd5aaf78f450128bb2c317f2b
yang cao <st186635@stud.uni-stuttgart.de>
2024-01-16 12:01:00 +0100

    Update eval.py

--- diabetic_retinopathy/evaluation/eval.py
+++ diabetic_retinopathy/evaluation/eval.py
@@ -5,12 +5,13 @@ import seaborn as sns
 import logging
 import wandb
 from evaluation.metrics import (recall_score, auc_score, f1score, accuracy_score,confusion_matrix)
+from absl.flags import FLAGS
 
 
-def evaluate(model: tf.keras.Model, checkpoint: object, ds_test: tf.data.Dataset, run_paths: dict) -> np.ndarray:
+def evaluate(model: tf.keras.Model, checkpoint: object, ds_test: tf.data.Dataset, run_paths: dict,ds_info) -> np.ndarray:
     ckpt = tf.train.Checkpoint(model=model, optimizer=tf.keras.optimizers.Adam())
     ckpt.restore(checkpoint).expect_partial()
-    wandb.init(project='idrid', name=run_paths['model_id'])
+    #wandb.init(project='idrid', name=run_paths['model_id'])
 
     true_labels = []
     pred_probs = []
@@ -24,27 +25,26 @@ def evaluate(model: tf.keras.Model, checkpoint: object, ds_test: tf.data.Dataset
     pred_labels = np.argmax(pred_probs, axis=1)
 
     # 使用自定义函数计算指标
-    conf_matrix = confusion_matrix(true_labels, pred_labels)
+    conf_matrix = confusion_matrix(true_labels, pred_labels,ds_info['num_classes'])
     accuracy = accuracy_score(true_labels, pred_labels)
-    sensitivity = recall_score(true_labels, pred_labels, 1)
-    specificity = recall_score(true_labels, pred_labels, 0)
-    auc = auc_score(true_labels, [pred[1] for pred in pred_probs])
-    f1_score = f1score(true_labels, pred_labels, 1)
-
-    # Log metrics
     logging.info("Confusion Matrix:\n%s", conf_matrix)
     logging.info("Accuracy: %s", accuracy)
-    logging.info("Sensitivity: %s", sensitivity)
-    logging.info("Specificity: %s", specificity)
-    logging.info("ROC/AUC: %s", auc)
-    logging.info("f1_score:%s", f1_score)
-
     wandb.log({"confusion_matrix": wandb.plot.confusion_matrix(probs=None, y_true=true_labels, preds=pred_labels,
-                                                               class_names=["Class 0", "Class 1"]),
-               "accuracy": accuracy,
-               "sensitivity": sensitivity,
-               "specificity": specificity,
-               "roc_auc": auc})
+                                                               class_names=[f"Class {i}" for i in range(ds_info['num_classes'])]),
+               "accuracy": accuracy})
+
+    # use flags to control the evaluation
+    if not FLAGS.multi_class:
+        sensitivity = recall_score(true_labels, pred_labels, 1)
+        specificity = recall_score(true_labels, pred_labels, 0)
+        auc = auc_score(true_labels, [pred[1] for pred in pred_probs])
+        f1_score = f1score(true_labels, pred_labels, 1)
+        logging.info("Sensitivity: %s", sensitivity)
+        logging.info("Specificity: %s", specificity)
+        logging.info("ROC/AUC: %s", auc)
+        logging.info("f1_score:%s", f1_score)
+        wandb.log({"sensitivity": sensitivity,"specificity": specificity,"roc_auc": auc})
+
 
     # Plot the confusion matrix
     plt.figure(figsize=(10, 8))
@@ -60,17 +60,16 @@ def evaluate(model: tf.keras.Model, checkpoint: object, ds_test: tf.data.Dataset
     return conf_matrix
 
 
-def evaluate1(model: tf.keras.Model, ds_test: tf.data.Dataset, run_paths) -> np.ndarray:
-    # Initialize Weights & Biases
+def evaluate1(model: tf.keras.Model, ds_test: tf.data.Dataset, run_paths,ds_info) -> np.ndarray:
     wandb.init(project='idrid', name=run_paths['model_id'])
 
     true_labels = []
     pred_probs = []
 
     for x, y in ds_test:
-        y_pred = model(x, training=False)  # 直接在模型上调用 x
+        y_pred = model(x, training=False)
         true_labels.extend(y.numpy())
-        pred_probs.extend(y_pred.numpy())  # 使用 numpy() 转换
+        pred_probs.extend(y_pred.numpy())
 
     true_labels = np.array(true_labels)
     pred_labels = np.argmax(pred_probs, axis=1)
@@ -78,25 +77,23 @@ def evaluate1(model: tf.keras.Model, ds_test: tf.data.Dataset, run_paths) -> np.
     # 使用自定义函数计算指标
     conf_matrix = confusion_matrix(true_labels, pred_labels)
     accuracy = accuracy_score(true_labels, pred_labels)
-    sensitivity = recall_score(true_labels, pred_labels, 1)
-    specificity = recall_score(true_labels, pred_labels, 0)
-    auc = auc_score(true_labels, [pred[1] for pred in pred_probs])
-    f1_score = (f1score(true_labels, pred_labels, 1))
-
-    # Log metrics
     logging.info("Confusion Matrix:\n%s", conf_matrix)
     logging.info("Accuracy: %s", accuracy)
-    logging.info("Sensitivity: %s", sensitivity)
-    logging.info("Specificity: %s", specificity)
-    logging.info("ROC/AUC: %s", auc)
-    logging.info("f1_score:%s", f1_score)
-
     wandb.log({"confusion_matrix": wandb.plot.confusion_matrix(probs=None, y_true=true_labels, preds=pred_labels,
-                                                               class_names=["Class 0", "Class 1"]),
-               "accuracy": accuracy,
-               "sensitivity": sensitivity,
-               "specificity": specificity,
-               "roc_auc": auc})
+                                                               class_names=[f"Class {i}" for i in range(ds_info['num_classes'])]),
+               "accuracy": accuracy})
+
+    if not FLAGS.multi_class:
+        sensitivity = recall_score(true_labels, pred_labels, 1)
+        specificity = recall_score(true_labels, pred_labels, 0)
+        auc = auc_score(true_labels, [pred[1] for pred in pred_probs])
+        f1_score = f1score(true_labels, pred_labels, 1)
+        logging.info("Sensitivity: %s", sensitivity)
+        logging.info("Specificity: %s", specificity)
+        logging.info("ROC/AUC: %s", auc)
+        logging.info("f1_score:%s", f1_score)
+        wandb.log({"sensitivity": sensitivity,"specificity": specificity,"roc_auc": auc})
+
 
     # Plot the confusion matrix
     plt.figure(figsize=(10, 8))
@@ -110,3 +107,4 @@ def evaluate1(model: tf.keras.Model, ds_test: tf.data.Dataset, run_paths) -> np.
     wandb.finish()
 
     return conf_matrix
+
